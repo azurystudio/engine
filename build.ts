a89532b3c6612a04175083e03ddd011c5cc93b47
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-03-06 20:26:20 +0100

    fix: do not parse cmd args

--- build.ts
+++ build.ts
@@ -9,9 +9,6 @@ import slash from 'https://esm.sh/slash@5.0.0?pin=v110'
 import { parseConfiguration } from './parseConfiguration.ts'
 import { parseRoute } from './parseRoute.ts'
 import version from './version.json' assert { type: 'json' }
-import { parse } from 'https://deno.land/std@v0.178.0/flags/mod.ts'
-
-const args = parse(Deno.args)
 
 const tmpDir = await Deno.makeTempDir({ prefix: 'darkflare-' }),
     config = await parseConfiguration(tmpDir)
@@ -36,7 +33,7 @@ const tmpDir = await Deno.makeTempDir({ prefix: 'darkflare-' }),
     await Deno.copyFile(path, dest)
   }
 
-  const srcUrl = `https://raw.githubusercontent.com/azurystudio/engine/${args.version ?? version}`
+  const srcUrl = `https://raw.githubusercontent.com/azurystudio/engine/${Deno.args[0] ?? version}`
 
   let workerString = (await (await fetch(`${srcUrl}/worker.ts`)).text())
       .replace(
