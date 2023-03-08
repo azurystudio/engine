d636236883574ae1d922fde10551e14c69876dc2
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-03-08 01:42:38 +0100

    bump engine to `v0.1.7`

--- engine.d.ts
+++ engine.d.ts
@@ -11,8 +11,7 @@ declare namespace Core {
 }
 
 declare const Schema: typeof import('./ext/Schema.ts').Schema
-declare const ObjectId: typeof import('./ext/ObjectId.ts').ObjectId
-declare type ObjectId = import('./ext/ObjectId.ts').ObjectId
+declare class ObjectId extends (await import('./ext/ObjectId.ts')).ObjectId {}
 
 declare type Configuration = import('./Configuration.d.ts').Configuration
 
